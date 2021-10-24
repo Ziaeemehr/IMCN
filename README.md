194b53a3332774a749d47684171ecb2f4f5aa639
Ziaeemehr <a.ziaeemehr@gmail.com>
2021-10-24 09:20:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# IMCN
+
+Information Measurements on Complex Networks
+
+### installation the latest changes
+
+```sh
+cd IMCN # directory where you have setup.py
+pip3 install .
+```
+
+
---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name='imcn',
+    version='0.0.1',
+    author="Abolfazl Ziaeemehr",
+    author_email="a.ziaeemehr@gmail.com",
+    description="Information measurements on complex networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ziaeemehr/IMCN",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    # package_data={'sbi_nmms': ['DampOscillator.so']},
+    install_requires=requirements,
+    # include_package_data=True,
+)
