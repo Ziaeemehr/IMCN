e97206d171e8cf572d67e4ab4193bede4ffcb30f
Ziaeemehr <a.ziaeemehr@gmail.com>
2021-10-25 18:02:23 +0200

    version updated.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("requirements.txt") as f:
 
 setuptools.setup(
     name='imcn',
-    version='0.0.1',
+    version='0.1',
     author="Abolfazl Ziaeemehr",
     author_email="a.ziaeemehr@gmail.com",
     description="Information measurements on complex networks",
